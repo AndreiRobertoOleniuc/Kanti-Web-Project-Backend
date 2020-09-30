1774074c1658799007e5bd1e33ca7ca2d1a70cdd
Andrei Roberto Oleniuc <andreioleniucroberto@gmail.com>
2020-10-01 00:02:35 +0200

    Calculate

--- backend/src/main/java/com/example/QuestionsAPI/QuestionController.java
+++ backend/src/main/java/com/example/QuestionsAPI/QuestionController.java
@@ -18,29 +18,30 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class QuestionController {
-
+    
     private static final String template = "%s?";
     private final AtomicLong counter = new AtomicLong();
     ArrayList<Question> questions = new ArrayList<>();
-
+    
     public QuestionController() {
         questions.add(new Question(1, "Hast du spass mit Computer zu arbeiten ?"));
         questions.add(new Question(2, "Wie läuft es so mit Mathe ?"));
         questions.add(new Question(3, "Willst du deine Freizeit Opfern ?"));
+        questions.add(new Question(4, "Könntest du dir vorstellen in ein Gebiet einzutauchen welches sich Konstant verändert?"));
     }
-
+    
     @GetMapping("/question")
     @CrossOrigin(origins = "http://localhost:3000")
     public Question question(@RequestParam(value = "question", defaultValue = "Wie gehts dir") String question) {
         return new Question(counter.incrementAndGet(), String.format(template, question));
     }
-
+    
     @GetMapping("/getAllQuestion")
     @CrossOrigin(origins = "http://localhost:3000")
     public ArrayList<Question> questions() {
         return questions;
     }
-
+    
     @GetMapping("/getQuestionID")
     @CrossOrigin(origins = "http://localhost:3000")
     public Question getQuestionId(@RequestParam(value = "id", defaultValue = "1") String id) {
@@ -52,5 +53,20 @@ public class QuestionController {
         }
         return new Question(404, "Question not Found");
     }
-
+    
+    @GetMapping("/calculateRate")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public Answer getCalculation(@RequestParam(value = "answers", defaultValue = "1111") String answer) {
+        ArrayList<Integer> werte = new ArrayList<>();
+        for (int i = 0; i < answer.length(); i++) {
+            int a = Integer.parseInt(String.valueOf(answer.charAt(i)));
+            werte.add(a);
+        }
+        double sum = 0;
+        for (Integer i : werte) {
+            sum += i;
+        }
+        sum /= 16;
+        return new Answer(1, Double.toString(sum));
+    }
 }
